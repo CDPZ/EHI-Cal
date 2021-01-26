816283cf4713173d692f652150615b3dafbbf71a
CDPZ <279871734@qq.com>
2021-01-27 06:36:10 +0800

    assist-remove frgout

---
+++ Auxiliary/calfragout.py
@@ -0,0 +1,12 @@
+import os 
+
+if __name__ == '__main__':
+
+    for i in range(6,111):
+        for j in range(200):
+            if i < 10:
+                file_data = u"Q:\\NEW(1)\\2002\\Folder 0" + str(i) + "\\frgout"+str(j)+".land"
+            else:
+                file_data = u"Q:\\NEW(1)\\2002\\Folder " + str(i) + "\\frgout"+str(j)+".land"
+            if os.path.isfile(file_data) == True:
+                os.remove(file_data)
