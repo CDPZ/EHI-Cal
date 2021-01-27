5e58944719ef10a08c8f1b533d0be44820b443be
Paul <49158663+CDPZ@users.noreply.github.com>
2021-01-27 19:35:42 +0800

    Update calfragout.py

--- Auxiliary/calfragout.py
+++ Auxiliary/calfragout.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     for i in range(6,111):
         for j in range(200):
             if i < 10:
-                file_data = u"Q:\\NEW(1)\\2002\\Folder 0" + str(i) + "\\frgout"+str(j)+".land"
+                file_data = u"Q:\\NEW(1)\\2002\\Folder 0" + str(i) + "\\fragout"+str(j)+".land"
             else:
-                file_data = u"Q:\\NEW(1)\\2002\\Folder " + str(i) + "\\frgout"+str(j)+".land"
+                file_data = u"Q:\\NEW(1)\\2002\\Folder " + str(i) + "\\fragout"+str(j)+".land"
             if os.path.isfile(file_data) == True:
                 os.remove(file_data)
